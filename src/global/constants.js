24d84d7636644ffbbc3843138c2550af93e0a44d
JY <84664024+jyydev@users.noreply.github.com>
2021-06-25 15:05:05 +0800

    init

---
+++ src/global/constants.js
@@ -0,0 +1,7 @@
+export const API_HOST = "http://localhost:3000"
+// export const API_GET_DATA = `${API_HOST}/posts/1`
+// export const API_GET_DATA = `https://my-json-server.typicode.com/jyydev/json/posts/1`;
+// export const API_GET_DATA = `https://raw.githubusercontent.com/jyydev/json/main/db2.json`;
+// export const API_GET_DATA = `https://my-json-server.typicode.com/MigueldeIcaza1/mockjson/db`;
+// export const API_GET_DATA = `https://api.jsonbin.io/b/60d55fb38ea8ec25bd1528cd`;
+export const API_GET_DATA = `https://jsonbin.org/jyydev/public/react-crash-course/posts/0`;
